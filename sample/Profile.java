5e8b25d22f52e8a217e8efa18538ea5c4d3c4cbe
jordin513 <jordin513@gmail.com>
2018-11-26 12:47:04 -0500

    Add files via upload
    Added current + target weight functionality

--- sample/Profile.java
+++ sample/Profile.java
@@ -1,3 +1,117 @@
+package sample;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Profile implements Initializable {
+
+    /* USER DATA TO BE COLLECTED FOR FUTURE USE */
+    private String name;
+    private String username;
+    private char sex;
+    private int age;
+    private String currentweight;
+    private String targetweight;
+
+    //FILE LOCATION -- Used in method to print to text file
+    private static final String FILENAME = "src/sample/UserInfo.txt";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCurrentWeight(String currentweight) {
+        this.currentweight = currentweight;
+    }
+
+    public String getCurrentWeight() {
+        return currentweight;
+    }
+
+    public void setTargetWeight(String targetweight) {
+        this.targetweight = targetweight;
+    }
+
+    public String getTargetWeight() {
+        return targetweight;
+    }
+
+    // Set user's sex and age on profile screen, and to display on choice box
+    ObservableList sexes = FXCollections.observableArrayList();
+    ObservableList ages = FXCollections.observableArrayList();
+
+    // workaround for populating an integer array with known values. Easier than hard-coding an array of integers
+    List<Integer> agesStream = IntStream.rangeClosed(13, 85).boxed().collect(Collectors.toList());
+
+    /***************************************************************
+     /* PROFILE'S ANCHOR PANE FXML ELEMENTS
+     ****************************************************************/
+
+    @FXML
+    private AnchorPane mainLoginScreen;
+
+    @FXML
+    private Button finalizeAcctButton;
+
+    @FXML
+    private Label acctText;
+
+    @FXML
+    private Label mainNameLabel;
+
+    @FXML
+    private TextField nameCollector;
 
     @FXML
     private Label ageLabel;
