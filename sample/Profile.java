365dceaf944db138289d5353b7aaf47358f1d789
jordin513 <jordin513@gmail.com>
2018-11-26 19:33:52 -0500

    Add files via upload
    Added Privacy Radio

--- sample/Profile.java
+++ sample/Profile.java
@@ -14,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -148,6 +149,10 @@ public class Profile implements Initializable {
 
     @FXML
     private TextField CurrentWeight;
+
+    @FXML
+    private RadioButton PrivacyRadio;
+
 
 
     Stage stageHere;
